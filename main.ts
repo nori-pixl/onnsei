182969c7eb0068d38f3ecab1919ac0cb6a356e34
nori-pixl <sato.shigure4@gmail.com>
2026-04-02 02:17:30 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,14 @@
-//% color="#4b0082" icon="\uf028" block="Speech"
+/**
+ * ここに書いた説明がツールボックスのヒントになります
+ */
+//% color="#4b0082" icon="\uf028" block="音声合成"
 namespace mySpeech {
     /**
-     * Pythonの speech.say(text, speed, pitch) を再現
-     * @param text 喋らせる英単語
-     * @param speed 話す速度 (0-255)
-     * @param pitch 声の高さ (0-255)
+     * 喋るブロック
      */
-    //% block="say %text | speed %speed | pitch %pitch"
-    //% text.defl="hello"
-    //% speed.min=0 speed.max=255 speed.defl=72
-    //% pitch.min=0 pitch.max=255 pitch.defl=64
-    export function say(text: string, speed: number, pitch: number): void {
-        // 内部エンジンの設定を更新
-        billy.configure(speed, pitch, 64, 128); 
-        // 発声実行
-        billy.say(text);
+    //% block="喋る %text"
+    export function say(text: string): void {
+        basic.showString(text)
     }
 }
+
